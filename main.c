64cd2dc939c58e7965f754844f2316135881939b
Lucas Rocha <lr359508@gmail.com>
2024-08-24 07:12:37 -0300

    fix: função principal corrigida e refatorada

--- main.c
+++ main.c
@@ -13,34 +13,36 @@ int main(){
 	gerar_tabuleiro(&navios_bot);
 
 	posicionar_navios(navios_jogador, navios_bot);
+	char linha_jogador, coluna_jogador, linha_bot, coluna_bot;
 	while(verificar_vitoria(ataques_jogador, navios_jogador, ataques_bot, navios_bot) == 3){
 		do{
 			do{
-				printf("Coluna: ");
-				scanf("%s", &coluna);
-				if(numero_coluna(coluna)==10){
-					printf("Valor inválido, digite apenas os valores no intervalo de A a J\n");
+				puts("Coluna:");
+				scanf("%c", &coluna_jogador);
+				if(numero_coluna(coluna_jogador)==10){
+					puts("Valor inválido, digite apenas os valores no intervalo de A a J");
 				}
-			}while(numero_coluna(coluna)==10);
+			}while(numero_coluna(coluna_jogador) == 10);
 
 			do{
-				printf("Linha: ");
-				scanf("%d", &linha);
-				linha-=1;
-				if(linha<0 || linha>9){
-					printf("Valor inválido, digite apenas os valores no intervalo de 0 a 9\n");
+				puts("Linha:");
+				scanf("%d", &linha_jogador);
+				if(linha_jogador < 1 || linha_jogador > 10){
+					puts("Valor inválido, digite apenas os valores no intervalo de 1 a 10");
 				}
-			}while(linha<0 || linha>9);
-			if(tabuleiro_bot[linha][numero_coluna(coluna)]!='-'){
-				printf("Casa já revelada, tente outra vez!\n");
+			}while(linha_jogador < 0 || linha_jogador > 9);
+			if(ataques_jogador.data[linha_jogador - 1][numero_coluna(coluna_jogador)] != 0){
+				puts("Casa já revelada, tente outra vez!");
 			}
-		}while(tabuleiro_bot[linha][numero_coluna(coluna)]!='-');
+		}while(ataques_jogador.data[linha_jogador - 1][numero_coluna(coluna_jogador)] != 0);
 		do{
-			coluna_bot=rand()%10;
-			linha_bot=rand()%10;
-		}while(tabuleiro_jogador[linha_bot][coluna_bot]!='-');
-		tabuleiro(linha, coluna, linha_bot, coluna_bot);
-
+			coluna_bot = rand()%10;
+			linha_bot = rand()%10;
+		}while(ataques_bot.data[linha_bot][coluna_bot] != 0);
+		puts("Seu tabuleiro:");
+		imprimir_tabuleiro(ataques_jogador);
+		puts("Tabuleiro do seu oponente:");
+		imprimir_tabuleiro(ataques_bot);
 	}
 
 	if(verifica_vitoria() == 1){
